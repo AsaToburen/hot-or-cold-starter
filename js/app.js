72f984ec6bfa2119d19c4b9caed3e23e469b5e78
Asa Toburen <atopettit@gmail.com>
2014-11-23 16:25:20 -0700

    added almost functional feedback

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
+$(document).ready( function() {
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -28,31 +27,39 @@ newGame();
 	}
 	
 	// Get guess from user
-	$('#guessButton').click('input', function() {
+	$('#guessButton').click('input', function () {
 	  		event.preventDefault();
-	  	if(('input' !== null) && ('input' !== NaN) && (1 < 'input' < 101)) {
+	  	if(('input' !== null) || ('input' !== NaN) || (1 < 'input' < 101)) {
 	  		var userGuess = parseInt($( 'input' ).val());
 	  		$('#count').text(++guessCount);
 	  		$('#guessList').append("<li>" + userGuess + "</li>");
 
-	  	function feedBack(userGuess) {
+	  	function guessResponse(userGuess) {
 			var distance = Math.abs(randomNumber - userGuess);
 			console.log(randomNumber, userGuess, distance);
 			if( userGuess == randomNumber) {
-				$('#userGuess'.val("Guessed Correctly.") }
-				}
-			}; 
-			feedBack(userGuess);
-	  	});
-
+				$('#feedback').text('You guessed it!');
+			};
+			if( distance >= 50) {
+				$('#feedback').text('Ice Cold');
+				} else if (30 <= distance < 50 ) {
+					$('#feedback').text('Cold');
+				  }	else if (20 <= distance < 30) {
+						$('#feedback').text('Warm');
+					}  else if (10 <= distance < 20) {
+							$('#feedback').text('Hot');
+					};
+				};
+			};
+	
+	  	guessResponse(userGuess);
 	//new game on click	
-	$('.new').on('click', function() {
+	$('.new').on('click', function () {
 		newGame();
-	 	})
+	 	});
 	
 	});
-	
-	
+});
 		 
 	 
 	

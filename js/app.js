e32e7de36957e40b72a9e5508a31d51e94bd8ee2
Asa Toburen <atopettit@gmail.com>
2014-11-23 15:12:57 -0700

    guess counter, random number added

--- js/app.js
+++ js/app.js
@@ -11,14 +11,50 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	
+//generate random number:
+newGame();
 
-   $('.new').on('click', function newGame() {	
-  		var genRand = Math.floor((Math.random() * 101));	
+	function genNumber () {
+		randomNumber = Math.floor((Math.random() * 100) + 1);
+		return randomNumber;
+ 		}
 
+	function newGame () {
+		guessCount = 0;
+		genNumber();
+		$('#guessList li').remove();
+		$('#count').text(guessCount);
+		$('#userGuess').val("");
+	}
+	
+	// Get guess from user
+	$('#guessButton').click('input', function() {
+	  		event.preventDefault();
+	  	if(('input' !== null) && ('input' !== NaN) && (1 < 'input' < 101)) {
+	  		var userGuess = parseInt($( 'input' ).val());
+	  		$('#count').text(++guessCount);
+	  		$('#guessList').append("<li>" + userGuess + "</li>");
 
-  		});
-  	});
+	  	function feedBack(userGuess) {
+			var distance = Math.abs(randomNumber - userGuess);
+			console.log(randomNumber, userGuess, distance);
+			if( userGuess == randomNumber) {
+				$('#userGuess'.val("Guessed Correctly.") }
+				}
+			}; 
+			feedBack(userGuess);
+	  	});
 
+	//new game on click	
+	$('.new').on('click', function() {
+		newGame();
+	 	})
+	
+	});
+	
+	
+		 
+	 
+	
 
 

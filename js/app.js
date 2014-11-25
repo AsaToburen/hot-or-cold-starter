1e43e02c2a39b7c7be02cad11293890bb7f47567
Asa Toburen <atopettit@gmail.com>
2014-11-25 08:36:00 -0700

    added if and else if statements to determine appropriate responds to user's guess

--- js/app.js
+++ js/app.js
@@ -13,10 +13,14 @@ $(document).ready( function() {
 //generate random number:
 newGame();
 
+//Generate Random Number
+
 	function genNumber () {
 		randomNumber = Math.floor((Math.random() * 100) + 1);
 		return randomNumber;
  		}
+
+//Create new Game, Reset all settings
 
 	function newGame () {
 		guessCount = 0;
@@ -24,30 +28,41 @@ newGame();
 		$('#guessList li').remove();
 		$('#count').text(guessCount);
 		$('#userGuess').val("");
+		$('#feedback').text('Make your Guess!');
 	}
 
+//Provide response to user's guess
 	function guessResponse(userGuess) {
 			var distance = Math.abs(randomNumber - userGuess);
 			console.log(randomNumber, userGuess, distance);
-			if( userGuess == randomNumber) {
+			if( userGuess === randomNumber) {
 				$('#feedback').text('You guessed it!');
-			};
+			}
+//Insert appropriate hot/cold hint, determined by userGuess proximity to random number.
 
 			if( distance >= 50) {
 				$('#feedback').text('Ice Cold');
-				} else if (30 <= distance < 50 ) {
-					$('#feedback').text('Cold');
-				  }	else if(20 <= distance < 30) {
-						$('#feedback').text('Warm');
-					}  else if (10 <= distance < 20) {
-							$('#feedback').text('Hot');
-					};
-				};
+				} else if( distance >= 50) {
+ 						$('#feedback').text('Ice Cold');
+ 					} else if (distance < 50 && distance > 30 ) {
+ 						$('#feedback').text('Cold');
+ 					} else if(distance < 30 && distance > 20) {
+ 						$('#feedback').text('Warm');
+ 					} else if (distance > 10 && distance < 20) {
+ 						$('#feedback').text('Hot');
+ 					} else if (distance > 5 && distance < 9) {
+ 						$('#feedback').text('BURNIN\'');
+ 					} else if (distance > 1 && distance < 5) {
+ 						$('#feedback').text('FIRE!!');
+ 					} else if (userGuess === randomNumber) {
+ 						$('#feedback').text('You guessed it!');
+ 					}
+ 						}
 	
-	// Get guess from user
+	// Get guess from user on button click.
 	$('#guessButton').click('input', function () {
 	  		event.preventDefault();
-	  	if(('input' !== null) || ('input' !== NaN) || (1 < 'input' < 101)) {
+	  	if(('input' !== null) || ( isNan('input')) || (1 < 'input' < 101)) {
 	  		var userGuess = parseInt($( 'input' ).val());
 	  		$('#count').text(++guessCount);
 	  		$('#guessList').append("<li>" + userGuess + "</li>");
@@ -55,13 +70,16 @@ newGame();
 
 	  	
 	
-	//new game on click	
+	//event handler to listen for click on reset button.
 	$('.new').on('click', function () {
 		newGame();
 	 	});
-};	
+}	
 });
-});
+	});
+
+		 
+	 
 		 
 	 
 	
